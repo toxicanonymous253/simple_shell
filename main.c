f2b5b27a8983313d2cac35d4283f79bed1a550f6
toxicfrank <ngugifrancis930@gmai.com>
2023-10-13 14:58:33 +0300

    simple_shell

--- main.c
+++ main.c
@@ -5,15 +5,28 @@
  * Return: 0 always when success 
  */
 
-int main(int argc, char **argv)
+int main(void)
 {
+	char buffer[100], *args, *token;
+	pid_t child_pid;
+	int i = 0;
+
 	while (1)
 	{
 		/*Display a prompt*/
 		printf("$ ");
 
 		/*read command inputed*/
-		
+		fgets(buffer, sizeof(buffer), stdin);
+
+		token = strtok(buffer, "\n");
+
+		while(token != NULL)
+		{
+			args[i] = token;/*assigning the tokenized commands*/
+			token = strtok(NULL, "\n");
+			i++;
+		}
 	}
 
 	return (0);
