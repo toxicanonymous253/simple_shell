3873b69c55644539b292a755445079840de39ea1
toxicfrank <ngugifrancis930@gmai.com>
2023-10-13 09:32:26 +0300

    beautiful-code

---
+++ beautiful_code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is a code that will pass all of the betty checks
+ * Return: 0 alwasy when code is successful
+ */
+
+int main(void)
+{
+	/*Print hello world*/
+	printf("Hello world!\n");
+
+	return (0);
+}
