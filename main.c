c96de0ac26de516167118c1586802b2263f92fdd
toxicfrank <ngugifrancis930@gmai.com>
2023-10-17 20:14:31 +0300

    simple_shell

--- main.c
+++ main.c
@@ -8,11 +8,15 @@ int main(void)
 {
 	char buffer[100];
 	pid_t child_pid;
+	int status;
+
 	while (1)
 	{
 		/*Display a prompt*/
 		printf("#cisfun$ ");
-		fgets(buffer, sizeof(buffer), stdin);/*Read the input and store it in buffer*/
+		fgets(buffer, sizeof(buffer), stdin);/*store stdin in buffer*/
+		if (feof(stdin))
+			break;
 		buffer[strcspn(buffer, "\n")] = '\0';/*Removing the \n from the command*/
 		child_pid = fork();/*starting child process*/
 		if (child_pid == -1)/*failed child process*/
@@ -23,8 +27,13 @@ int main(void)
 		if (child_pid == 0)/*successfull child process*/
 		{
 			char *args[] = {buffer, NULL};
-			execve();
+
+			execve(buffer, args, environ);
+			perror("./shell");/*If execeve fails*/
+			exit(1);
 		}
+		else
+			wait(&status);/*Wait for child to exit*/
 	}
 	return (0);
 }
