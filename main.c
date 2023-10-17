9901ed23ddbc3b69627db8612c7e55b0e153549b
toxicfrank <ngugifrancis930@gmai.com>
2023-10-17 19:34:45 +0300

    simpl_shell

--- main.c
+++ main.c
@@ -6,61 +6,6 @@
  */
 int main(void)
 {
-	char *user_input, *args[32], *command, *command_copy, *directory, full_path[100];
-	int status,arg_count = 0;
-	pid_t child_pid;
-	command = getenv("PATH");
-	command_copy = strdup(command);
-	user_input = (char *)malloc(100 * sizeof(char));
-	if (user_input == NULL)
-	{
-		perror("malloc");
-		exit(1);
-	}
-
-	while (1)
-	{
-		printf("#cisfun$ ");/*Displays a prompt*/
-		fgets(user_input, sizeof(user_input), stdin);/*reading thse users input*/
-
-		user_input[strcspn(user_input, "\n")] = '\0';/*removing the new line*/
-		command = strtok(user_input, " ");
-
-		if (command == NULL)
-			continue;
-		if (strcmp(command, "exit") == 0)
-		{
-			free(user_input);
-			break;
-		}
-		directory = strtok(command_copy, ":");
-		while (directory != NULL)
-		{
-			sprintf(full_path, "%s/%s", directory, command);
-			if (fileExists(full_path))/*Checkin if executable exists*/
-			{
-				child_pid = fork();
-				
-				if (child_pid == -1)
-				{
-					perror("fork");
-					exit(1);
-				}
-				if (child_pid == 0)
-				{
-					args[arg_count] = command;
-					while ((args[arg_count] = strtok(NULL, " ")) != NULL)
-						arg_count++;
-					execve(full_path, args, environ);
-					perror("execve");
-					exit(1);
-				}
-				else
-					wait(&status);
-			}
-			directory = strtok(NULL, ":");
-		}
-	}
-	free(command_copy);
+	printf("Hello world\n");
 	return (0);
 }
